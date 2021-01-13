25c3c67e965b2af2023b6ffc14c342c1920effea
Max Block <maxblock@pm.me>
2021-01-13 13:15:20 +0300

    ui: simple_form

--- app/server/routers/ui_router.py
+++ app/server/routers/ui_router.py
@@ -1,19 +1,30 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends, Request
 from mb_commons import md
 from mb_commons.mongo import make_query
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 from wtforms import BooleanField, Form, IntegerField, SelectField, StringField
+from wtforms.validators import URL
 
 from app.core.core import Core
-from app.core.models import DataStatus
+from app.core.models import DataStatus, WorkerCreate
 from app.server.jinja import form_choices
+
+
+async def get_form_data(request: Request):
+    return await request.form()
 
 
 class WorkersFilterForm(Form):
     name = StringField(default="")
     started = BooleanField()
     limit = IntegerField(default=100)
+
+
+class WorkerCreateForm(Form):
+    name = StringField()
+    source = StringField(validators=[URL()])
+    interval = IntegerField()
 
 
 class DataFilterForm(Form):
@@ -36,10 +47,24 @@ def init(core: Core, templates: Jinja2Templates) -> APIRouter:
         workers = core.db.worker.find(query, "-created_at", form.data["limit"])
         return templates.TemplateResponse("workers.j2", md(request, form, workers))
 
+    @router.get("/create-worker")
+    def create_worker_page(request: Request):
+        form = WorkerCreateForm()
+        return templates.TemplateResponse("create_worker.j2", md(request, form))
+
     @router.get("/data", response_class=HTMLResponse)
     def data_page(request: Request):
         form = DataFilterForm(request.query_params)
-        data = core.db.data.find({}, "-created_at", form.data["limit"])
+        query = make_query(worker=form.data["worker"], status=form.data["status"])
+        data = core.db.data.find(query, "-created_at", form.data["limit"])
         return templates.TemplateResponse("data.j2", md(request, form, data))
+
+    @router.post("/create-worker")
+    def create_worker_action(form_data=Depends(get_form_data)):
+        form = WorkerCreateForm(form_data)
+        if form.validate():
+            return core.worker_service.create(WorkerCreate(**form.data))
+        else:
+            return {"error": form.errors}
 
     return router
